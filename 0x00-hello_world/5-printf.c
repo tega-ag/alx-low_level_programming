3476f61eefb15e1f4113184db558e6eed9a1fb29
tega-ag <agadagbatega@yahoo.com>
2022-09-08 17:53:55 -0700

    Hello, puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints is like builing a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	print("with proper grammer, but the outcome is a piece of art,\n")
+	return (0);
+ }
