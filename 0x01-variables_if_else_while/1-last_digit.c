363cb4494ad564503ca1bf4aca5da2038326e86d
tega-ag <agadagbatega@yahoo.com>
2022-09-09 18:48:12 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,21 +12,21 @@
 int main(void)
 {
 int n;
-int 1;
+int l;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-1 = n % 10;
+l = n % 10;
 
 if (n > 5)
 { 
-	printf("Last digit of %d and is greater than 5\n", n, 1);                                                                               
+	printf("Last digit of %d and is greater than 5\n", n, l);                                                                               
 }                                                                                                                                               
 else if (1 == 0)                                                                                                                                
 {                                                                                                                                              
-	printf("Last digit of %d and is %d and is 0\n", n, 1);                                                                                  
+	printf("Last digit of %d and is %d and is 0\n", n, l);                                                                                  
 }                                                                                                                                               
 else                                                                                                                                            
 {                                                                                                                                         
-	printf("Last digit of %d and is %d and is less than 6 and not 0\n", n, 1);                                                             
+	printf("Last digit of %d and is %d and is less than 6 and not 0\n", n, l);                                                             
 }		                                                                                                                               	return (0);                 
