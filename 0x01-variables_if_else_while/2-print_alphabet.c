a36f7625e3d06cffbc11d0255842635294a0d983
tega-ag <agadagbatega@yahoo.com>
2022-09-12 14:25:35 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
-* main - print if the number is postive, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int n;
-int l;
+	char love;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-
-if (l > 5)
-{
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-	printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else
-{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
+	for (love = 'a'; love <= 'z'; love++)
+		putchar(love);
+	putchar('\n');
+	return (0);
 }
